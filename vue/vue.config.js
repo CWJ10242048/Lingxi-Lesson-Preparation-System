c1724b57fcf3665cf535b99c0cd67e44cb0ff43b
CWJ10242048 <2518106470@qq.com>
2025-03-13 22:26:17 +0800

    最初始的版本，把别人的项目代码部署运行成功之后传了上去而已。
    最初始的版本，把别人的项目代码部署运行成功之后传了上去而已。

---
+++ vue/vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    publicPath: './',
+    productionSourceMap: true,
+    devServer:{
+        port:80,
+        host:'localhost',
+        open:true,
+        // proxy: {
+        //     '/api': {
+        //         target: 'http://localhost:8080/',
+        //         ws: true,
+        //         changeOrigin: true,
+        //         // pathRewrite: {
+        //         //     '^/api': ''  //通过pathRewrite重写地址，将前缀/api转为/
+        //         // }
+        //     }
+        // }
+    }
+}
